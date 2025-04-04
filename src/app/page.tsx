b56f5f3cb0d856088db6f5f4d94f7b9b83cbf6d5
ashutosh <ashutoshsahni833@gmail.com>
2025-04-05 00:56:01 +0530

    removed_dublicate_calendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,6 @@ export default function Home() {
       <ComparisonSection />
       <ServicesSection />
       <FAQSection items={faqItems} />
-      <CalendlyWidget />
       <ContactForm />
     </>
   );
